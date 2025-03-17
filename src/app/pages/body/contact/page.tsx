7a08adbeb7687997363982d2f5138ce63c547dd9
purbaronny <ronny.purba@yahoo.co.id>
2025-03-17 15:55:21 +0700

    feat: contact

--- src/app/pages/body/contact/page.tsx
+++ src/app/pages/body/contact/page.tsx
@@ -7,8 +7,18 @@ export default function BodyContact() {
             {/* Location Section */}
             <div id="contactLocationId" className="col-span-12 md:col-span-2 flex flex-col items-center text-center">
                 <h2 className="text-[#414141] text-2xl font-semibold leading-[72px]">Location</h2>
-                <div id="contactLocationGoogleMap" className="w-[360px] h-[148px] bg-gray-300 mt-4">
-                    {/* Ganti dengan Google Maps embed */}
+                <div id="contactLocationGoogleMap" className="w-[360px] h-[148px] mt-4">
+                    <iframe
+                        title="Google Maps - PT. Dapur Rumah Sejahtera"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d989.004345593481!2d112.7115502!3d-7.2793774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fc088fcf92f1%3A0xf6bf8b906b7e02b2!2sPt%20Dapur%20Rumah%20Sejahtera!5e0!3m2!1sen!2sid!4v1710432912345"
+                        width="360"
+                        height="148"
+                        style={{ border: 0 }}
+                        allowFullScreen={true}
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                        className="w-full h-full"
+                    ></iframe>
                 </div>
             </div>
 
