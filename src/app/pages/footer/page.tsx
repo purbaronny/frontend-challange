b29e276424e926d6b848ea981351fc6ac72f33e1
purbaronny <ronny.purba@yahoo.co.id>
2025-03-18 23:22:07 +0700

    Update page.tsx

--- src/app/pages/footer/page.tsx
+++ src/app/pages/footer/page.tsx
@@ -6,7 +6,9 @@ export default function Footer() {
     <footer className="bg-white shadow-md p-4">
       <div className="container mx-auto flex items-center justify-between px-4 md:px-8 lg:px-12">
         <div className="flex items-center">
-          <p className="h-10">&copy; 2024 Twiscode Pte. Ltd. | PT. Dapur Rumah Sejahtera</p>
+          <p className="h-10 text-sm font-sans text-gray-600">
+            &copy; 2024 Twiscode Pte. Ltd. | PT. Dapur Rumah Sejahtera
+          </p>
         </div>
       </div>
     </footer>
