8dc1a3400d1df57fd1fb3316ff2d9957e822bbd9
purbaronny <ronny.purba@yahoo.co.id>
2025-03-19 13:57:06 +0700

    feat: almost finish

--- src/app/lib/profile.tsx
+++ src/app/lib/profile.tsx
@@ -1,63 +1,39 @@
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3001";
+
+async function fetchData(endpoint: string) {
+    const res = await fetch(`${API_URL}/${endpoint}`);
+    if (!res.ok) throw new Error(`Failed to fetch ${endpoint}`);
+    return res.json();
+}
+
 export async function getMenus() {
-  const res = await fetch("http://localhost:3001/menus");
-  if (!res.ok) throw new Error("Failed to fetch menus");
-  return res.json();
+    return fetchData("menus");
 }
 
 export async function getBoxes() {
-  const res = await fetch("http://localhost:3001/boxes");
-  if (!res.ok) throw new Error("Failed to fetch boxes");
-  return res.json();
+    return fetchData("boxes");
 }
 
 export async function getServices() {
-  const res = await fetch("http://localhost:3001/services");
-  if (!res.ok) throw new Error("Failed to fetch services");
-  return res.json();
+    return fetchData("services");
 }
 
 export async function getImages() {
-  const res = await fetch("http://localhost:3001/images");
-  if (!res.ok) throw new Error("Failed to fetch images");
-  return res.json();
+    return fetchData("images");
 }
 
 export async function getTestimonials() {
-  const res = await fetch("http://localhost:3001/testimonials");
-  if (!res.ok) throw new Error("Failed to fetch testimonials");
-  return res.json();
+    return fetchData("testimonials");
 }
 
 export async function getTeamMembers() {
-  const res = await fetch("http://localhost:3001/teamMembers");
-  if (!res.ok) throw new Error("Failed to fetch teamMembers");
-  return res.json();
+    return fetchData("teamMembers");
 }
 
-// export async function getMapLocation(): Promise<string | null> {
-//   try {
-//       const res = await fetch("http://localhost:3001/mapLocation");
-
-//       if (!res.ok) {
-//           throw new Error(`HTTP error! Status: ${res.status}`);
-//       }
-
-//       const data = await res.text(); // Karena API mengembalikan string
-//       return data;
-//   } catch (error) {
-//       console.error("Error fetching map location:", error);
-//       return null; // Kembalikan `null` jika terjadi error
-//   }
-// }
-
 export async function getMapLocation() {
-  const res = await fetch("http://localhost:3001/mapLocation");
-  if (!res.ok) throw new Error("Failed to fetch mapLocation");
-  return res.json();
+    return fetchData("mapLocation");
 }
 
 export async function getContacts() {
-  const res = await fetch("http://localhost:3001/contacts");
-  if (!res.ok) throw new Error("Failed to fetch contacts");
-  return res.json();
+    return fetchData("contacts");
 }
